5637ffce055c54b7ab1f73a1ef6add0b4cb91ab9
Adzky <adzkyms@gmail.com>
2026-03-04 13:25:50 +0700

    remove checkStatus again

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -20,6 +20,6 @@ router.post("/notification", handleNotification);
 // ===============================
 // Endpoint untuk cek status transaksi (opsional)
 // ===============================
-router.get("/status/:order_id", checkStatus);
+//router.get("/status/:order_id", checkStatus);
 
 export default router;
