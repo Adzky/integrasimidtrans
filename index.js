e817bd0cfd7eea9a76433c0ef49a8e5a09ea0c0f
Adzky <adzkyms@gmail.com>
2026-03-04 13:21:14 +0700

    edit index.js for serverless

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ app.get("/", (req, res) => {
 // Routes
 app.use("/payment", paymentRoutes);
 
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server running on http://localhost:${PORT}`);
+// });
